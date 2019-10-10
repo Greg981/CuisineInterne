0743dcddf4e9cf7c2e971abd7acdc85e7de5f372
Greg981 <gregory.grosse@outlook.com>
2019-10-10 14:49:17 +0200

    Ajout modification 10oct2019

---
+++ exos/bar/bar.js
@@ -0,0 +1,19 @@
+let bonjour = document.getElementById('b1');// bouton cliquez moi
+let ajouter = document.getElementById('b2');//bouton paragraphe
+
+bonjour.addEventListener('click', alerte);// creation de l'alerte 
+ajouter.addEventListener('click', ajout);// creation de l'ajout
+
+function alerte() {
+    alert('bonjour Comment allez vous ?'); //message du pop up
+}
+function ajout() { // boite ajout paragrhape
+    let para = document.createElement('p');
+    para.textContent = 'Le paragraphe a bien été ajouté, ça prouve que ca fonctionne ';
+    document.body.appendChild(para);
+}
+
+
+$(function () { // scrip pour le selecteur de langue
+    $('.selectpicker').selectpicker();
+});
