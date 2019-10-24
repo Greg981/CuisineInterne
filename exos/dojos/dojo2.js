190d7f0649332ea39c1f730a0998b571f2408ead
Greg981 <gregory.grosse@outlook.com>
2019-10-24 16:42:20 +0200

    Ajout modification 24oct2019

---
+++ exos/dojos/dojo2.js
@@ -0,0 +1,7 @@
+let bonjour = document.getElementById('b1');// bouton cliquez moi
+
+bonjour.addEventListener('click', alerte);// creation de l'alerte 
+
+function alerte() {
+    alert('Vous avez réellement cru que le robot allez disparaitre ??? PERDU!!!! J\'y travaille, mais c\'est pas encore pour tout de suite'); //message du pop up
+}
